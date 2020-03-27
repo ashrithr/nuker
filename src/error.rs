518cb942ed607e1691160e04c79eac6764d4931e
ashrithr <ashrith@me.com>
2020-03-27 10:47:43 -0700

    Add error wrapper around Rusoto errors

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,43 @@
+use rusoto_core::{request::BufferedHttpResponse, RusotoError};
+use serde::Deserialize;
+use tracing::debug;
 
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct ErrorResponse {
+    error: ErrorResponseError,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+struct ErrorResponseError {
+    code: String,
+    message: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Error<E> {
+    Rusoto(RusotoError<E>),
+    Throttling(String),
+    Validation(String),
+}
+
+impl<E> From<RusotoError<E>> for Error<E> {
+    fn from(err: RusotoError<E>) -> Self {
+        match &err {
+            RusotoError::Unknown(BufferedHttpResponse { ref body, .. }) => {
+                if let Ok(ErrorResponse { error }) =
+                    serde_xml_rs::from_reader::<_, ErrorResponse>(body.as_ref())
+                {
+                    match error.code.as_str() {
+                        "Throttling" => return Error::Throttling(error.message),
+                        "ValidationError" => return Error::Validation(error.message),
+                        code => debug!("unmatched error code {}", code),
+                    }
+                }
+                Error::Rusoto(err)
+            }
+            _ => Error::Rusoto(err),
+        }
+    }
+}
